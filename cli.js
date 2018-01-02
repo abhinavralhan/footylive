6484e0c6b0a80bad72a941588f8cbb990825b227
abhinav ralhan <abhinav.ralhan1@gmail.com>
2018-01-03 02:43:21 +0530

    added all

---
+++ cli.js
@@ -0,0 +1,283 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const yargs = require('yargs');
+const fs = require('fs');
+const ora = require('ora');
+const chalk = require('chalk');
+const request = require('request');
+const moment = require('moment');
+const inquirer = require('inquirer');
+const Table = require('cli-table');
+const config = require('./config');
+const leagueIds = require('./leagueIds');
+const helpers = require('./helpers');
+const path = require('path');
+
+/**
+ * Get all helpers from `helpers.js`
+ */
+const fixturesHelper = helpers.fixturesHelper;
+const getURL = helpers.getURL;
+const refresh = helpers.refresh;
+const scoresHelper = helpers.scoresHelper;
+const standingsHelper = helpers.standings;
+const updateMessage = helpers.updateMessage;
+
+/**
+ * Headers for every request that is made
+ */
+const headers = {
+  'X-Auth-Token': config.API_KEY,
+};
+
+/**
+ * Command line interface code for the app
+ */
+const argv = yargs
+  .usage('$0 <command>')
+  .command('scores', 'Get scores of past and live fixtures', (yargsScores) => {
+    /**
+     * Get all the options set for `scores` command
+     */
+    const scores = yargsScores
+      .usage('Usage: $0 scores [options]')
+      .alias('l', 'live')
+        .describe('l', 'Live scores')
+        .boolean('l')
+      .alias('t', 'team')
+        .describe('t', 'Select team')
+        .string('t')
+      .example('$0 scores -t "Manchester United" -l')
+      .argv;
+
+    const spinner = ora('Fetching data').start();
+
+    const team = (scores.t === undefined) ? '' : (scores.t).toLowerCase();
+
+    /**
+     * timeFrameStart Set start date from which fixtures is to be fetch
+     * timeFrameEnd   Set end date till which fixtures is to be fetch
+     * End Point for fetching all fixtures between `timeFrameStart` and `timeFrameEnd`
+     */
+    const timeFrameStart = moment().subtract(1, 'days').format('YYYY-MM-DD');
+    const timeFrameEnd = moment().add(1, 'days').format('YYYY-MM-DD');
+    const url = `fixtures?timeFrameStart=${timeFrameStart}&timeFrameEnd=${timeFrameEnd}`;
+
+    /**
+     * Creates request to fetch fixtures and show them
+     * @param  {String} options."url":     getURL(url) End point from where data
+     *                                                    needs to be fetched
+     * @param  {Object} options."headers": headers     Headers for the request
+     * @return {None}                                  None
+     */
+    request({ url: getURL(url), headers }, (err, res, body) => {
+      if (err) {
+        spinner.stop();
+        updateMessage('REQ_ERROR');
+      } else {
+        spinner.stop();
+        scoresHelper(scores.l, team, body);
+      }
+    });
+  })
+  .command('fixtures', 'Get upcoming and past fixtures of a league and team', (yargsFixtures) => {
+    /**
+     * Get all the options set for `fixtures` command
+     */
+    const fixtures = yargsFixtures
+      .usage('Usage: $0 fixtures [options]')
+      .alias('d', 'days')
+        .describe('t', 'Number of days')
+      .alias('l', 'league')
+        .describe('l', 'League')
+      .alias('t', 'team')
+        .describe('t', 'Team name or substring of it')
+        .string('t')
+      .alias('n', 'next')
+        .describe('n', 'Next or upcoming matches')
+        .boolean('n')
+      .example('$0 fixtures -l PL -d 5 -t "Manchester United" -n')
+      .argv;
+
+    const spinner = ora('Fetching data').start();
+
+    /**
+     * days   Number of days for which data needs to be fetched
+     * league League code for which data needs to be fetched
+     * team   Team for which fixtures is requested
+     * time   Past or present depending on flag `n` set
+     */
+    const days = fixtures.d || 10;
+    const league = fixtures.l;
+    const team = fixtures.t || '';
+    const time = (fixtures.n === true) ? 'n' : 'p';
+
+    if (days < 0) {
+      updateMessage('FIX_INPUT_ERR');
+    }
+
+    /**
+     * timeFrame Combination of `time` and `days` as per API requirements
+     * @type {String}
+     */
+    const timeFrame = `${time}${days}`;
+
+    if (league !== undefined) {
+      if (leagueIds[league] === undefined) {
+        spinner.stop();
+        updateMessage('LEAGUE_ERR');
+      }
+
+      const id = leagueIds[league].id;
+      const name = leagueIds[league].caption;
+
+      request({
+        url: getURL(`competitions/${id}/fixtures?timeFrame=${timeFrame}`),
+        headers,
+      }, (err, res, body) => {
+        if (err) {
+          spinner.stop();
+          updateMessage('REQ_ERROR');
+        } else {
+          spinner.stop();
+          fixturesHelper(league, name, team, body);
+        }
+      });
+    } else {
+      request({
+        url: getURL(`fixtures?timeFrame=${timeFrame}`),
+        headers,
+      }, (err, res, body) => {
+        if (err) {
+          spinner.stop();
+          updateMessage('REQ_ERROR');
+        } else {
+          spinner.stop();
+          fixturesHelper(league, undefined, team, body);
+        }
+      });
+    }
+  })
+  .command('standings', 'Get standings of particular league', (yargsStandings) => {
+    /**
+     * Get all the options set for `standings` command
+     */
+    const standings = yargsStandings
+      .usage('Usage: $0 standings [options]')
+      .alias('l', 'league').describe('l', 'League to be searched').demand('l')
+      .example('$0 standings -l PL')
+      .argv;
+
+    const spinner = ora('Fetching data').start();
+
+    const league = standings.l;
+
+    if (leagueIds[league] === undefined) {
+      spinner.stop();
+      updateMessage('LEAGUE_ERR');
+    }
+
+    const id = leagueIds[league].id;
+
+    request({
+      url: getURL(`competitions/${id}/leagueTable`),
+      headers,
+    }, (err, res, body) => {
+      if (err) {
+        spinner.stop();
+        updateMessage('REQ_ERROR');
+      } else {
+        spinner.stop();
+        standingsHelper(body);
+      }
+    });
+  })
+  .command('lists', 'List of codes of various competitions', (yargsLists) => {
+    /**
+     * Get all the options set for `lists` command
+     */
+    const lists = yargsLists
+      .usage('Usage: sudo $0 lists [options]')
+      .alias('r', 'refresh')
+        .describe('r', 'Refresh league ids')
+        .boolean('r')
+      .example('sudo $0 lists -r')
+      .argv;
+
+    const spinner = ora('Fetching data').start();
+
+    if (lists.r) {
+      request({
+        url: getURL('competitions'),
+        headers,
+      }, (err, res, body) => {
+        if (err) {
+          spinner.stop();
+          updateMessage('REQ_ERROR');
+        } else {
+          spinner.stop();
+          const newLeagueIDs = refresh(body);
+          fs.writeFileSync(
+            path.resolve(__dirname, 'leagueIds.json'),
+            JSON.stringify(newLeagueIDs, null, 2),
+            'utf8'
+          );
+          updateMessage('UPDATE', 'New list fetched and saved');
+        }
+      });
+    } else {
+      const table = new Table({
+        head: [
+          chalk.bold.white.bgCyan('League'),
+          chalk.bold.white.bgCyan('League Code'),
+        ],
+        colWidths: [40, 20],
+      });
+
+      for (let league of Object.keys(leagueIds)) {
+        table.push([
+          chalk.bold.cyan(leagueIds[league].caption),
+          chalk.bold.green(league),
+        ]);
+      }
+      spinner.stop();
+      console.log(table.toString());
+    }
+  })
+  .command('config', 'Change configuration and defaults', (yargsConfig) => {
+    /**
+     * Get all the options set for `config` command
+     */
+    const configs = yargsConfig
+      .usage('Usage: sudo $0 config')
+      .example('sudo $0 config')
+      .argv;
+
+    if (configs.h) {
+      return;
+    }
+
+    const questions = [{
+      type: 'input',
+      name: 'API_KEY',
+      message: 'Enter API KEY <leave blank incase unchanged>',
+    }];
+
+    inquirer.prompt(questions).then((answers) => {
+      const obj = config;
+
+      if (answers.API_KEY !== '') {
+        obj.API_KEY = answers.API_KEY;
+      }
+
+      fs.writeFileSync(path.resolve(__dirname, 'config.json'), JSON.stringify(obj, null, 2), 'utf8');
+      updateMessage('UPDATE', 'API KEY has been updated.');
+    }).catch((err) => {
+      updateMessage('CUSTOM_ERR', 'Please run the following command with root access');
+    });
+  })
+  .help('h')
+  .alias('h', 'help')
+  .argv;
---
+++ helpers.js
@@ -0,0 +1,243 @@
+'use strict';
+
+const leagueIds = require('./leagueIds');
+const Table = require('cli-table');
+const chalk = require('chalk');
+const moment = require('moment');
+const URLS = require('./constants');
+
+const API_URL = URLS.API_URL;
+const BUGS_URL = URLS.BUGS_URL;
+
+const buildScore = (name, homeTeam, goalsHomeTeam, goalsAwayTeam, awayTeam, time) => (
+  `${chalk.green.bold(name)}  ${chalk.cyan.bold(homeTeam)} ${chalk.cyan.bold(goalsHomeTeam)} vs. ` +
+  `${chalk.red.bold(goalsAwayTeam)} ${chalk.red.bold(awayTeam)} ${chalk.yellow.bold(time)}`
+);
+
+const updateMessage = (TYPE, message) => {
+  message = message || '';
+
+  switch (TYPE) {
+
+    case 'REQ_ERROR':
+      console.log(
+        chalk.red.bold(`Sorry, an error occured. Please report issues to ${BUGS_URL} if problem persists.`)
+      );
+      break;
+
+    case 'CUSTOM_ERR':
+      console.log(chalk.bold.red(message));
+      break;
+
+    case 'UPDATE':
+      console.log(chalk.bold.cyan(message));
+      break;
+
+    case 'LEAGUE_ERR':
+      throw new Error(chalk.red.bold(
+        'No league found. Please check the League Code entered with the list `football lists`.'
+      ));
+
+    case 'FIX_INPUT_ERR':
+      throw new Error(chalk.red.bold('Days cannot be a negative value.'));
+
+    default:
+      console.log('ERROR OCCURED.');
+  }
+};
+
+const getLeagueName = (fixture) => {
+  let compUrl = fixture._links.competition.href;
+  let parts = compUrl.split('/');
+  let id = parts[parts.length - 1];
+
+  for (let league in leagueIds) {
+    if (leagueIds[league].id === id) {
+      return leagueIds[league].caption;
+    }
+  }
+
+  return '';
+};
+
+const fixturesHelper = (league, name, team, body) => {
+  let data = JSON.parse(body);
+  let fixtures = data.fixtures;
+
+  if ('error' in data) {
+    updateMessage('CUSTOM_ERR', data.error);
+    return;
+  }
+
+  if (fixtures.length === 0) {
+    updateMessage('UPDATE', 'Sorry, no fixtures to show right now');
+    return;
+  }
+
+  for (let fixture of fixtures) {
+    let homeTeam = fixture.homeTeamName;
+    let awayTeam = fixture.awayTeamName;
+    let goalsHomeTeam = (fixture.result.goalsHomeTeam === null) ? '-1' : fixture.result.goalsHomeTeam;
+    let goalsAwayTeam = (fixture.result.goalsAwayTeam === null) ? '-1' : fixture.result.goalsAwayTeam;
+
+    name = (league === undefined) ? getLeagueName(fixture) : name;
+
+    let time = (fixture.status === 'IN_PLAY') ? 'LIVE' : moment(fixture.date).calendar();
+
+    if (team !== undefined) {
+      if ((homeTeam.toLowerCase()).indexOf((team).toLowerCase()) !== -1 ||
+          (awayTeam.toLowerCase()).indexOf((team).toLowerCase()) !== -1) {
+        console.log(buildScore(name, homeTeam, goalsHomeTeam, goalsAwayTeam, awayTeam, time));
+      }
+    } else {
+      console.log(buildScore(name, homeTeam, goalsHomeTeam, goalsAwayTeam, awayTeam, time));
+    }
+  }
+};
+
+const getStandingsTableInstance = () => (
+  new Table({
+    head: [
+      chalk.bold.white.bgCyan('Rank'),
+      chalk.bold.white.bgCyan('Team'),
+      chalk.bold.white.bgCyan('Played'),
+      chalk.bold.white.bgCyan('Goal Diff'),
+      chalk.bold.white.bgCyan('Points'),
+    ],
+    colWidths: [7, 25, 10, 15, 10],
+  })
+);
+
+const getURL = endPoint => API_URL + endPoint;
+
+const printScores = (fixtures, isLive) => {
+  for (let fixture of fixtures) {
+    let name = getLeagueName(fixture);
+    let homeTeam = fixture.homeTeamName;
+    let awayTeam = fixture.awayTeamName;
+    let goalsHomeTeam = (fixture.result.goalsHomeTeam === null) ? '-1' : fixture.result.goalsHomeTeam;
+    let goalsAwayTeam = (fixture.result.goalsAwayTeam === null) ? '-1' : fixture.result.goalsAwayTeam;
+    let time = (isLive === true) ? 'LIVE' : moment(fixture.date).calendar();
+
+    console.log(buildScore(name, homeTeam, goalsHomeTeam, goalsAwayTeam, awayTeam, time));
+  }
+};
+
+const refresh = (body) => {
+  let data = JSON.parse(body);
+  let newLeagueIDs = {};
+
+  if ('error' in data) {
+    updateMessage('CUSTOM_ERR', data.error);
+    return null;
+  }
+
+  for (let comp of data) {
+    newLeagueIDs[comp.league] = {
+      id: comp.id,
+      caption: comp.caption
+    };
+  }
+
+  return newLeagueIDs;
+};
+
+const scoresHelper = (isLive, team, body) => {
+  let data = JSON.parse(body);
+  let fixtures = data.fixtures;
+  let live = [];
+  let scores = [];
+
+  if ('error' in data) {
+    updateMessage('CUSTOM_ERR', data.error);
+    return;
+  }
+
+  team = team.toLowerCase();
+
+  for (let fixture of fixtures) {
+    let homeTeam = (fixture.homeTeamName).toLowerCase();
+    let awayTeam = (fixture.awayTeamName).toLowerCase();
+
+    if (fixture.status === 'IN_PLAY' && (homeTeam.indexOf(team) !== -1 ||
+                                         awayTeam.indexOf(team) !== -1)) {
+      live.push(fixture);
+      scores.push(fixture);
+    } else if (fixture.status === 'FINISHED' && (homeTeam.indexOf(team) !== -1 ||
+                                                 awayTeam.indexOf(team) !== -1)) {
+      scores.push(fixture);
+    }
+  }
+
+  if (isLive) {
+    if (live.length !== 0) {
+      printScores(live, true);
+    } else {
+      updateMessage('UPDATE', 'Sorry, no live match right now');
+    }
+  } else {
+    if (scores.length !== 0) {
+      printScores(scores, false);
+    } else {
+      updateMessage('UPDATE', 'Sorry, no scores to show right now');
+    }
+  }
+};
+
+const standings = (body) => {
+  let data = JSON.parse(body);
+  let table;
+
+  if ('error' in data) {
+    updateMessage('CUSTOM_ERR', data.error);
+    return;
+  }
+
+  if (data.standing !== undefined) {
+    let standing = data.standing;
+
+    table = getStandingsTableInstance();
+
+    for (let team of standing) {
+      table.push([
+        chalk.bold.magenta(team.position),
+        chalk.bold.cyan(team.teamName),
+        chalk.bold.yellow(team.playedGames),
+        chalk.bold.blue(team.goalDifference),
+        chalk.bold.green(team.points),
+      ]);
+    }
+
+    console.log(table.toString());
+  } else {
+    let groupStandings = data.standings;
+
+    for (let groupCode in groupStandings) {
+      console.log(chalk.bgCyan.bold.white(groupCode));
+
+      let group = standings[groupCode];
+
+      table = getStandingsTableInstance();
+
+      for (let team of group) {
+        table.push([
+          chalk.bold.magenta(team.rank),
+          chalk.bold.cyan(team.team),
+          chalk.bold.yellow(team.playedGames),
+          chalk.bold.blue(team.goalDifference),
+          chalk.bold.green(team.points),
+        ]);
+      }
+      console.log(table.toString());
+    }
+  }
+};
+
+module.exports = {
+  fixturesHelper,
+  getURL,
+  refresh,
+  scoresHelper,
+  standings,
+  updateMessage,
+};
